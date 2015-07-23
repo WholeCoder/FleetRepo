421065034755fce864e8816d924846a77cd70833
WholeCoder <m2gt@at>
2015-07-23 09:53:15 -0400

    Added word 'Done' to end of chart percentages in barchart.

--- index.js
+++ index.js
@@ -295,9 +295,9 @@ app.post("/barchartdata", function(req, res) {
   // [0,13],[1,11],[2,15],[3,15],[4,18],[5,21],[6,28]
   ];
   var percents2 = [
-      [0, "10%"],
-      [1, "50%, 75%, 90%"],
-      [2, "100%"]];
+      [0, "10% Done"],
+      [1, "50%, 75%, 90% Done"],
+      [2, "100% Done"]];
 
   var barchartdata = {"wins": wins2, "percents": percents2};
 
