d2390e51bb22aa9d4a9c7ca8ae254eb33ac60f57
WholeCoder <m2gt@at>
2015-10-11 18:23:33 -0400

    Removed dist folder.

--- dist/config-model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var ConfigSchema = new Schema({
-    nextsenddailyemail: { type: Date }
-});
-
-
-module.exports = mongoose.model('Config', ConfigSchema);
--- dist/lot-walkthrough-instance-model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var LotWalkthroughInstanceSchema = new Schema({
-    dateoflotwalkthrough: { type: Date }
-});
-
-
-module.exports = mongoose.model('LotWalkthroughInstance', LotWalkthroughInstanceSchema);
