99f22502b20a16ffd9002e61514092d07595f164
WholeCoder <m2gt@at>
2015-12-01 16:57:23 -0500

    Minified vendor files to cut in half their download size.

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,24 @@ module.exports = function(grunt) {
         src: ['public/newassets/**/*.js',
               '!**/public/newassets/js/vendor/**'],
         dest: 'dist/<%= pkg.name %>_newassest_javascript.js'
+      },
+      distVendor: {
+        /* **WARKING**  BE SURE TO ADD THE JAVASCRIPT FILES TO THIS THAT ARE IN THE INDEX.HTML FILE!!! */
+        src: ['public/newassets/js/vendor/jquery.js',
+              'public/newassets/js/vendor/jquery-ui-1.10.3.js',
+              'public/newassets/js/vendor/json2.js',
+              'public/newassets/js/vendor/underscore.js',
+              'public/newassets/js/vendor/backbone.js',
+              'public/newassets/js/vendor/backbone.picky.js',
+              'public/newassets/js/vendor/backbone.syphon.js',
+              'public/newassets/js/vendor/backbone.marionette.js',
+              'public/newassets/js/vendor/spin.js',
+              'public/newassets/js/vendor/spin.jquery.js',
+              'public/newassets/js/vendor/jquery.tablesorter.combined.js',
+              'public/newassets/js/vendor/datepicker/js/bootstrap-datepicker.js',
+              'public/newassets/js/vendor/datepicker/locales/bootstrap-datepicker.el.min.js'
+              ],
+        dest: 'dist/<%= pkg.name %>_vendor_javascript.js'
       }
     },
     uglify: {
@@ -50,6 +68,11 @@ module.exports = function(grunt) {
       dist: {
         files: {
           'dist/public/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      },
+      distVendor: {
+        files: {
+          'dist/public/<%= pkg.name %>_vendor.min.js': ['<%= concat.distVendor.dest %>']
         }
       }
     }
@@ -63,24 +86,49 @@ module.exports = function(grunt) {
     var foundEndTemplateMarket = false;
     var concatenatedMinifiedFileScriptTagAlready = false;
 
+    var foundVendorStartTemplateMarker = false;
+    var foundVendorEndTemplateMarket = false;
+    var concatenatedMinifiedVendorFileScriptTagAlready = false;
+
     for (var i = 0; i < fileRay.length; i++)
     {
-      if(fileRay[i].indexOf('<!-- ReplaceTheseWthMinVersion-Start -->') > 0)
+
+      // This is for the vendor insert code.
+      if(fileRay[i].indexOf('<!-- ReplaceTheseWithMinVersionForVendor-Start -->') >= 0)
       {
-        foundStartTemplateMarker = true;
+        console.log('!!!!!!!!!!!!!!! STARTING TO REPLACE VENDOR MIN');
+        foundVendorStartTemplateMarker = true;
       }
-      if(fileRay[i].indexOf('<!-- ReplaceTheseWthMinVersion-End -->') > 0)
+      if(fileRay[i].indexOf('<!-- ReplaceTheseWithMinVersionForVendor-End -->') >= 0)
       {
-        foundEndTemplateMarket = true;
+        console.log('!!!!!!!!!!!!!!!!!!!ENDING REPLACE VENDOR NIM');
+        foundVendorEndTemplateMarket = true;
       }
 
-      if (foundStartTemplateMarker && !foundEndTemplateMarket && !concatenatedMinifiedFileScriptTagAlready)
+      if (foundVendorStartTemplateMarker && !foundVendorEndTemplateMarket && !concatenatedMinifiedVendorFileScriptTagAlready)
       {
-        outputTemplate += '<script src="./FleetRepo.min.js"></script>';  
-        concatenatedMinifiedFileScriptTagAlready = true;
-      } else if (foundStartTemplateMarker == foundEndTemplateMarket)
+        console.log('inserting FleetRepo_vendor.min.js-------------------');
+        outputTemplate += '<script src="./FleetRepo_vendor.min.js"></script>';  
+        concatenatedMinifiedVendorFileScriptTagAlready = true;
+      } else if (foundVendorStartTemplateMarker == foundVendorEndTemplateMarket)
       {
-        outputTemplate += fileRay[i];
+        if(fileRay[i].indexOf('<!-- ReplaceTheseWthMinVersion-Start -->') >= 0)
+        {
+          foundStartTemplateMarker = true;
+        }
+        if(fileRay[i].indexOf('<!-- ReplaceTheseWthMinVersion-End -->') >= 0)
+        {
+          foundEndTemplateMarket = true;
+        }
+
+        if (foundStartTemplateMarker && !foundEndTemplateMarket && !concatenatedMinifiedFileScriptTagAlready)
+        {
+          outputTemplate += '<script src="./FleetRepo.min.js"></script>';  
+          concatenatedMinifiedFileScriptTagAlready = true;
+        } else if (foundStartTemplateMarker == foundEndTemplateMarket)
+        {
+          outputTemplate += fileRay[i];
+        }
       }
     }
 
