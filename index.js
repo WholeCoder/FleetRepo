e4d8bf1b4a3cdb4af46c96575b677df13ba0ba4a
Ruben <rpierich@hotmail.com>
2016-02-15 12:05:01 -0500

    Changed database password.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (DISABLE_SSL && ENVIRONMENT == 'local_development') // on development
   mongodbconnectionstring = "mongodb://dbuser:ubuntu2rbnue3@ds047802.mongolab.com:47802/heroku_dswxx1s9";
 } else if (ENVIRONMENT == 'production') {
   require('newrelic');
-  mongodbconnectionstring = "mongodb://dbuser:ubuntu2rbnue3@ds027293-a0.mongolab.com:27293,ds027293-a1.mongolab.com:27293/heroku_qlr988hb?replicaSet=rs-ds027293";
+  mongodbconnectionstring = "mongodb://newdatabaseuser:qwertyuiop@ds027293-a0.mongolab.com:27293,ds027293-a1.mongolab.com:27293/heroku_qlr988hb?replicaSet=rs-ds027293";
 } else {
   console.log("!!CONFIG ERROR - ENVIRONMENT system variable not found.  Can not set mongodb variable!!!!")
 }
